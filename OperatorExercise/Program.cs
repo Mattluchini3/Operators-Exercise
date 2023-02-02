24de54a80224fdf2a0f70f830d389eb5be231c76
Matthew Luchini <luch@Matthews-MacBook-Pro.local>
2023-02-02 16:01:04 -0500

    OperatorsEX

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,38 @@
 ﻿namespace OperatorExercise
 {
+    //Exercise 1
+    //Addition: x + y
+    //Subtraction: x - y//
+    //Multiplication: x* y
+    //Division: x / y
+    //Modulus: x % y
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+
+            //Exercise 2
+            Console.WriteLine($"{a}/{b} is {quotient} remainder  {remainder}");
+
+            Console.WriteLine("Please enter the radius of your circle:");
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
+        }
+
+
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
         }
     }
 }
